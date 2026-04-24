5c17eac80da7174794ea996573b334c20b58d8ef
angelh187 <angelh187@nycstudents.net>
2026-04-24 10:16:38 -0400

    erhrshrw

--- angel.py
+++ angel.py
@@ -1,13 +1,13 @@
 import requests
 
 def cats(cats):
-    response = requests.get(f"https://cataas.com/cat/:tag")
+    select = input("What type of cat do u want?")
+    response = requests.get(f"https://cataas.com/cat/{select}")
     if response.status_code != 200:
         print("Please hold!")
         return None
     data = response.json()
+    print(data)
     return{
-        "color": data["tag"]
+        "description": data["tag"]
     }
-catss = cats("Orange")
-print(catss)
