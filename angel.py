a1b058e2e0beb21c830f22ec1f06d7a0ff00ba4d
angelh187 <angelh187@nycstudents.net>
2026-04-29 09:34:24 -0400

    rhwrh

--- angel.py
+++ angel.py
@@ -1,38 +1,38 @@
 import requests
 import tkinter as tk
+from PIL import Image, ImageTk
+
+def cats():
+    url = 'https://cataas.com/cat'
+    print(url)
+    response = requests.get(url)
+    with open('cat.jpeg', 'wb') as f:
+        f.write(response.content)
+
+    image = Image.open("cat.jpeg")
+    photo = ImageTk.PhotoImage(image)
+
+    label = tk.Label(window, image=photo)
+    label.pack(pady = 30)
+
 
 window = tk.Tk()
 window.title("message reverser")
-window.geometry("400x250")
+window.geometry("1000x600")
 window.resizable(False, False)
 prompt = tk.Label(window,text="Click the button for a cat!",font =("Arial,16"))
 prompt.pack(pady=10)
+
 button = tk.Button(
     window,
     text="Cat here",
-    command=,
+    command=cats,
     font = ("Arial",16),
     bg = "pink",
-    fg = "hot pink"
+    fg = "blue",
     relief="raised",
     padx=10, pady=5
     )
+button.pack(pady=20)
 
 window.mainloop()
-
-
-
-""" def cats(cats):
-    select = input("What type of cat do u want?")
-    response = requests.get(f"https://cataas.com/cat/{select}")
-    if response.status_code != 200:
-        print("Please hold!")
-        return None
-    data = response.json()
-    print(data)
-    return{
-        "description": data["tag"]
-    }
-kitty = cats(input)
-for key, value in kitty.items():
-    print(f"{key.title()}: {value}") """
